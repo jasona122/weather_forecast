7bcb813b338bb4d718bc5c26ca29f149d0218acb
Jason A <17jgunawan@gmail.com>
2021-01-13 21:34:06 +0700

    Added humidity data; refactored code for displaying data

--- main.js
+++ main.js
@@ -1796,15 +1796,55 @@ class WeatherForecast {
 
 class WeatherDisplay {
     constructor() {
-        this.elementId = "weatherDisplay";
+        this.displayArea = document.getElementById("forecastDisplayArea");
+    }
+
+    createCard(cardNumber) {
+        // Create main outer card div
+        let cardOuter = document.createElement("div");
+        cardOuter.style.width = "18rem"
+        cardOuter.classList.add("card");
+        cardOuter.id = "day" + cardNumber;
+
+        // Create inner card body div
+        let cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+
+        // Create card title (day of forecast)
+        let cardTitle = document.createElement("h5");
+        cardTitle.classList.add("card-title");
+        cardTitle.id = "titleDay" + cardNumber;
+
+        // Create card description (description of forecast)
+        let cardDesc = document.createElement("p");
+        cardDesc.classList.add("card-text");
+        cardDesc.id = "descDay" + cardNumber;
+
+        // Create unordered list to store forecast data
+        let cardDataList = document.createElement("ul");
+        cardDataList.classList.add("list-group");
+        cardDataList.classList.add("list-group-flush");
+        cardDataList.id = "dataDay" + cardNumber;
+
+        // Attach all elements to card body
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardDesc);
+        cardBody.appendChild(cardDataList);
+
+        // Attach card body to main outer card div
+        cardOuter.appendChild(cardBody);
+
+        // Attach card to main display area
+        this.displayArea.appendChild(cardOuter);
     }
 
     display(dataArray) {
-        let displayArea = document.getElementById(this.elementId);
-
-        // Populate cards by looping through
+        // Create and populate cards by looping through
         for(let i = 0; i < dataArray.length; i++) {
             let forecast = dataArray[i];
+
+            // Create card to display data
+            this.createCard(i);
 
             // Add title data (date and day)
             let forecastDate = new Date(forecast.date);
@@ -1832,7 +1872,7 @@ class WeatherDisplay {
             temperatureRow.innerHTML += forecast.temperature.low + " - " + forecast.temperature.high + "&deg;C";
             dataTable.appendChild(temperatureRow);
 
-            //Add wind data
+            // Add wind data
             let windRow = document.createElement("li");
             windRow.classList.add("list-group-item");
             // Apply wind icon
@@ -1840,6 +1880,13 @@ class WeatherDisplay {
             windRow.innerHTML += forecast.wind.direction + " | " + forecast.wind.speed.low + " - " + forecast.wind.speed.high + " km/h";
             dataTable.appendChild(windRow);
 
+            // Add humidity data
+            let humidityRow = document.createElement("li");
+            humidityRow.classList.add("list-group-item");
+            // Apply humidity icon
+            humidityRow.innerHTML = '<i class="fas fa-tint"></i> '
+            humidityRow.innerHTML += forecast.relative_humidity.low + " - " + forecast.relative_humidity.high + "%";
+            dataTable.appendChild(humidityRow);
         }
     }
 }
@@ -1849,12 +1896,14 @@ function initScreen() {
     let weather = new WeatherForecast();
     let display = new WeatherDisplay();
     weather.forecast().then(function(response) {
+        console.log(response.data);
         display.display(response.data.items[0].forecasts);
+    },
+    function(err) {
+        // On error, log the error reason
+        console.log(err);
     });
 }
 
-initScreen();
-
-//window.onload = initScreen;
-
+window.onload = initScreen;
 },{"axios":2}]},{},[29]);
